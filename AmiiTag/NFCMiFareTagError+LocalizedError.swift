c03552afeddb4f583d37a85866325c6c3ea30617
Daniel <790119+DanTheMan827@users.noreply.github.com>
2021-02-15 20:33:11 -0600

    Add localized error for NFCMiFareTagError

---
+++ AmiiTag/NFCMiFareTagError+LocalizedError.swift
@@ -0,0 +1,21 @@
+//
+//  NFCMiFareTagError+LocalizedDescription.swift
+//  AmiiTag
+//
+//  Created by Daniel Radtke on 2/15/21.
+//  Copyright © 2021 Daniel Radtke. All rights reserved.
+//
+
+import Foundation
+extension NFCMiFareTagError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .crcError: return "CRC Error"
+        case .eepromWriteError: return "EEPROM Write Error"
+        case .invalidArgument: return "Invalid Argument"
+        case .invalidAuthentication: return "Invalid Authentication"
+        case .invalidData: return "Invalid Data"
+        case .unknownError: return "Unknown Error"
+        }
+    }
+}
